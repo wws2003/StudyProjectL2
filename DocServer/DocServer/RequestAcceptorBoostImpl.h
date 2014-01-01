b1444b43a56db33c7c14a0b2978ff14de816e61b
wws2003 <wws2003@phams-MacBook-Pro.local>
2014-01-01 12:24:40 +0900

    test commit from ide

---
+++ DocServer/DocServer/RequestAcceptorBoostImpl.h
@@ -0,0 +1,20 @@
+//
+//  RequestAcceptorBoostImpl.h
+//  DocServer
+//
+//  Created by wws2003 on 1/1/14.
+//  Copyright (c) 2014 wws2003. All rights reserved.
+//
+
+#ifndef __DocServer__RequestAcceptorBoostImpl__
+#define __DocServer__RequestAcceptorBoostImpl__
+
+#include <iostream>
+
+#include "IRequestAcceptor.h"
+
+class RequestAcceptorBoostImpl {
+    
+};
+
+#endif /* defined(__DocServer__RequestAcceptorBoostImpl__) */
